06335f4adbe767c5b5691749aae980ebb7cc4786
Luka Gagnidze <luka.gagnidze.1@btu.edu.ge>
2020-08-07 14:19:24 +0400

    Suffixing app with built in logging library java.util.package

---
+++ SuffixingAppLogger/src/main/java/org/example/Config.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.*;
+
+import static org.example.Main.logr;
+
+
+public class Config {
+    InputStream inputStream;
+
+
+    public Properties getPropValues() {
+
+        Properties prop = new Properties();
+        String propFileName = "Config.properties";
+        try {
+            logr.log(Level.INFO, "Started loading resources from config resources file");
+            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+                logr.log(Level.INFO, "loading resources from config resources file has finished");
+
+            } else {
+                throw new FileNotFoundException();
+            }
+        } catch (IOException ex) {
+            logr.log(Level.SEVERE, " Property file " + propFileName + " not found in the classpath", ex);
+            ex.printStackTrace();
+        }
+        return prop;
+    }
+}
