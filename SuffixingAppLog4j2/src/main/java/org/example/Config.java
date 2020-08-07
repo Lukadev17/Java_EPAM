1c0b482d47b08f6acb3007d6d42a1dd88fc5e539
Luka Gagnidze <luka.gagnidze.1@btu.edu.ge>
2020-08-07 14:14:42 +0400

    SuffixingApp with log4j2 logger

---
+++ SuffixingAppLog4j2/src/main/java/org/example/Config.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+
+import static org.example.Main.logr;
+
+public class Config {
+    InputStream inputStream;
+
+    public Properties getPropValues() {
+        Properties prop = new Properties();
+        String propFileName = "log4j2.properties";
+        logr.info( "Started loading resources from config resources file");
+        try {
+            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+            if (inputStream != null) {
+                prop.load(inputStream);
+                logr.info("loading resources from config resources file has finished");
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+            }
+        } catch (IOException ex) {
+            logr.fatal( " Property file " + propFileName + " not found in the classpath", ex);
+            ex.printStackTrace();
+        }
+        return prop;
+    }
+}
